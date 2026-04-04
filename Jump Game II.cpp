2bca65b06d399dde4ee86c63d340bd1247c62e7a
Manik Tyagi <152266833+manik0635@users.noreply.github.com>
2026-04-04 18:44:12 +0530

    Implement jump game solution in Jump Game II.cpp

---
+++ Jump Game II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps=0, end=0, far=0;
+
+        for(int i=0;i<nums.size()-1;i++){
+            far=max(far,i+nums[i]);
+            if(i==end){
+                jumps++;
+                end=far;
+            }
+        }
+        return jumps;
+    }
+};
