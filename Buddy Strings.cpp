932e73634a7cba0bb41367afa4dcfa1008ee3255
Manik Tyagi <152266833+manik0635@users.noreply.github.com>
2026-04-27 23:04:31 +0530

    Add buddyStrings function in Buddy Strings.cpp
    Implement buddyStrings function to check if two strings can be considered buddy strings.

---
+++ Buddy Strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if(s.size() != goal.size()) return false;
+
+        if(s == goal) {
+            vector<int> f(26, 0);
+            for(char c : s) if(++f[c - 'a'] > 1)return true;
+            return false;
+
+        }
+
+        vector<int> diff;
+        for (int i =0; i < s.size(); i++)
+        if(s[i] != goal[i]) diff.push_back(i);
+
+        return diff.size() == 2 &&
+               s[diff[0]] == goal[diff[1]] &&
+               s[diff[1]] == goal[diff[0]];
+    }
+};
