c49e36530313c5c811e0fa8552768929fd7625be
Manik Tyagi <152266833+manik0635@users.noreply.github.com>
2026-04-28 22:42:04 +0530

    Implement Gray Code generation algorithm

---
+++ Gray Code.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> res{0};
+        for (int i = 0; i < n; i++) {
+            int sz = res.size();
+            for (int j = sz - 1; j >= 0; j--)
+                res.push_back(res[j] | (1 << i));
+        }
+        return res;
+    }
+};
