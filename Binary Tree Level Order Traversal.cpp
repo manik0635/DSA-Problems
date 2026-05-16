6a3397ff2ffcfaf14b1e6276d717dac50d6964f3
Manik Tyagi <152266833+manik0635@users.noreply.github.com>
2026-05-16 23:31:44 +0530

    Add level order traversal function in Solution class
    Implement level order traversal for binary tree.

---
+++ Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (!root) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> v;
+
+            while (n--) {
+                TreeNode* t = q.front();
+                q.pop();
+
+                v.push_back(t->val);
+
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
+            }
+
+            ans.push_back(v);
+        }
+
+        return ans;
+    }
+};
